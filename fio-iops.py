8b2052a0be09382f9f3f55b35cb98896055caedd
speedtoys <65208176+speedtoys@users.noreply.github.com>
2023-08-08 15:24:16 -0700

    Add files via upload

--- fio-iops.py
+++ fio-iops.py
@@ -1,63 +1,41 @@
 #!/usr/bin/python3
 
 import pandas as pd
-import numpy as np
-import argparse
 import matplotlib.pyplot as plt
 
-# Set up command-line argument parsing
-parser = argparse.ArgumentParser(description='Process a CSV file.')
-parser.add_argument('-i', '--input', type=str, help='The path to the input file')
-parser.add_argument('-o', '--output', type=str, help='The path to the output file')
-args = parser.parse_args()
+# Load the data and drop unnecessary columns
+data_iops = pd.read_csv("./iopslog_iops.log", sep=",\s*", engine='python', header=None)
+data_iops.drop([2, 3, 4], axis=1, inplace=True)
+data_iops.columns = ['Time', 'Value']
 
-# Load the data
-df = pd.read_csv(args.input, header=None)
+# Round down the values in the first column to the nearest thousand
+data_iops['Time'] = (data_iops['Time'] // 1000) * 1000
 
-# Remove columns 3, 4, and 5 (Python uses 0-based indexing, so these are columns 2, 3, 4)
-df = df.drop(df.columns[[2, 3, 4]], axis=1)
+# Sum the values for each 'Time'
+summed_data_iops = data_iops.groupby('Time')['Value'].sum()
 
-# Rename the remaining columns
-df.columns = ['Numbers', 'Values']
+# Drop rows where the time is not evenly divisible by 5000
+summed_data_iops = summed_data_iops[summed_data_iops.index % 5000 == 0]
 
-# Round down the numbers to the nearest thousand
-df['Numbers'] = (df['Numbers'] // 1000) * 1000
+# Find the range for the vertical axis
+vmin_iops = summed_data_iops.min() - 500
+vmax_iops = summed_data_iops.max() + 500
 
-# Extract unique values from the 'Numbers' column
-unique_numbers = df['Numbers'].unique()
-
-# Create a new dataframe with these unique values as columns
-new_df = pd.DataFrame(columns=unique_numbers)
-
-# For each unique number, get the corresponding values and place them in the new dataframe
-for num in unique_numbers:
-    # Get the values corresponding to the current number
-    values = df[df['Numbers'] == num]['Values'].values
-    
-    # Add these values to the corresponding column in the new dataframe
-    new_df[num] = pd.Series(values)
-
-# Keep only the columns that are evenly divisible by 5000
-columns_to_keep = [col for col in new_df.columns if col % 5000 == 0]
-new_df = new_df[columns_to_keep]
-
-# Save the new dataframe to a CSV file
-new_df.to_csv(args.output, index=False)
-
-# Create a line chart
+# Create the line chart
 plt.figure(figsize=(10, 6))
-for i in range(len(new_df)):
-    plt.plot(new_df.columns, new_df.iloc[i])
+plt.plot(summed_data_iops.index / 1000, summed_data_iops, marker='o')
 
+# Set the labels and title
 plt.xlabel('Time In MS Since Start')
-plt.ylabel('Row Values')
-plt.title('Line Chart')
+plt.ylabel('IOPS')
+plt.title('IOPS Over Time')
 
-# Set the vertical axis range based on the data
-lowest_value = new_df.min().min() - 30
-highest_value = new_df.max().max() + 30
-plt.ylim(lowest_value, highest_value)
+# Set the range for the vertical axis
+plt.ylim([vmin_iops, vmax_iops])
 
-# plt.legend()  # Do not present a legend
-plt.savefig('chart.png')
+# Do not display a legend
+plt.gca().legend_ = None
+
+# Show the plot
+plt.show()
 
